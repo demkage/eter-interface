1be98acf921b6f90c1b1826f90708b2644f0b970
abosii <alexandru.bosii@endava.com>
2017-04-12 15:42:32 +0300

    add runtime retention

--- docker-interface/src/main/java/com/eter/docker/qualifier/Active.java
+++ docker-interface/src/main/java/com/eter/docker/qualifier/Active.java
@@ -4,10 +4,13 @@ package com.eter.docker.qualifier;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.CONSTRUCTOR, ElementType.FIELD,
     ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 @Qualifier
 public @interface Active {
 }
